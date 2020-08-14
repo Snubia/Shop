17f62caa74a0b60370db148c98366851116aba16
Sandrine Babila Nubia <sandrine@Sandrines-MacBook-Pro.local>
2020-08-13 21:19:49 -0700

    added port

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
 const Order = require('./models/order');
 const OrderItem = require('./models/order-item');
-
+var port = process.env.PORT || 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -20,18 +20,20 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   User.findByPk(1)
-    .then(user => {
+    .then((user) => {
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -41,46 +43,46 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {
   constraints: true,
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, {
-  through: CartItem
+  through: CartItem,
 });
 Product.belongsToMany(Cart, {
-  through: CartItem
+  through: CartItem,
 });
 Order.belongsTo(User);
 User.hasMany(Order);
 Order.belongsToMany(Product, {
-  through: OrderItem
+  through: OrderItem,
 });
 
 sequelize
   // .sync({ force: true })
   .sync()
-  .then(result => {
+  .then((result) => {
     return User.findByPk(1);
     // console.log(result);
   })
-  .then(user => {
+  .then((user) => {
     if (!user) {
       return User.create({
         name: 'Nunu',
-        email: 'test@test.com'
+        email: 'test@test.com',
       });
     }
     return user;
   })
-  .then(user => {
+  .then((user) => {
     // console.log(user);
     return user.createCart();
   })
-  .then(cart => {
-    app.listen(3000);
+  .then((cart) => {
+    app.listen(port);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
