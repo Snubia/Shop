646d163a107ca4149de1f51eba12bafa26390d33
Sandrine Babila Nubia <sandrine@Sandrines-MacBook-Pro.local>
2020-08-13 20:45:47 -0700

    deplooying to heroku

--- data/util/database.js
+++ data/util/database.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize');
+var sequelize;
 
-const sequelize = new Sequelize('shop_app', 'root', 'password', {
-  dialect: 'mysql',
-  host: 'localhost',
-
-});
-
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize('shop_app', 'root', 'password', {
+    dialect: 'mysql',
+    host: 'localhost'
+  });
+};
 module.exports = sequelize;
