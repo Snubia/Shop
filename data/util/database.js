b42e239c54086257ea467804395a0240638dcf6d
Sandrine Babila Nubia <sandrine@Sandrines-MacBook-Pro.local>
2020-08-13 22:19:46 -0700

    added port

--- data/util/database.js
+++ data/util/database.js
@@ -8,7 +8,14 @@ if (process.env.JAWSDB_URL) {
   console.log('11111111111111111111111111111');
 } else if (JAWSDB) {
   console.log('workwoekosjsbsdhfkjbjsgvfjvjEfvjfdvadvjfdahbfdkjh');
-  sequelize = new Sequelize(process.env.JAWSDB);
+  sequelize = new Sequelize({
+    host: 'rnr56s6e2uk326pj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'xq41muiqops11r29',
+    password: 's5fz7x2t9iz1zd6c',
+    database: 'qfooekrkvoba9t8x',
+    dialect: 'mysql',
+    port: '3306',
+  });
 } else {
   console.log('1111111111111111111111111111188888888888888888');
   sequelize = new Sequelize('shop_app', 'root', 'password', {
