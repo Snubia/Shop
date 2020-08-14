3bf7b4a2373631c476ef1e084fdf323bd9342cd2
Sandrine Babila Nubia <sandrine@Sandrines-MacBook-Pro.local>
2020-08-13 21:30:23 -0700

    try

--- data/util/database.js
+++ data/util/database.js
@@ -2,11 +2,13 @@ const Sequelize = require('sequelize');
 var sequelize;
 
 if (process.env.JAWSDB_URL) {
+  console.log('11111111111111111111111111111');
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  console.log('1111111111111111111111111111188888888888888888');
   sequelize = new Sequelize('shop_app', 'root', 'password', {
     dialect: 'mysql',
-    host: 'localhost'
+    host: 'localhost',
   });
-};
+}
 module.exports = sequelize;
